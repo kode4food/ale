eb200de9ea3080018d04b7cc3616cec6b046dd30
Thomas Bradford <thom@kode4food.it>
2019-04-18 09:03:36 +0200

    beginning to restore coverage

--- bootstrap/builtin/basics_test.go
+++ bootstrap/builtin/basics_test.go
@@ -22,3 +22,61 @@ func TestRead(t *testing.T) {
 	as.True(ok)
 	as.Integer(3, v3)
 }
+
+func TestEmptyRead(t *testing.T) {
+	as := assert.New(t)
+	r1 := builtin.Read(S(""))
+	as.Nil(r1)
+}
+
+func TestRaise(t *testing.T) {
+	as := assert.New(t)
+
+	defer func() {
+		if rec := recover(); rec != nil {
+			as.String("blowed up!", rec)
+			return
+		}
+		as.Fail("error not raised")
+	}()
+
+	builtin.Raise(S("blowed up!"))
+}
+
+func TestRecover(t *testing.T) {
+	as := assert.New(t)
+	var triggered = false
+	builtin.Recover(
+		api.Call(func(_ ...api.Value) api.Value {
+			builtin.Raise(S("blowed up!"))
+			return S("wrong")
+		}),
+		api.Call(func(args ...api.Value) api.Value {
+			as.String("blowed up!", args[0])
+			triggered = true
+			return api.Nil
+		}),
+	)
+	as.True(triggered)
+}
+
+func TestDefer(t *testing.T) {
+	as := assert.New(t)
+	var triggered = false
+
+	defer func() {
+		as.True(triggered)
+		recover()
+	}()
+
+	builtin.Defer(
+		api.Call(func(_ ...api.Value) api.Value {
+			builtin.Raise(S("blowed up!"))
+			return S("wrong")
+		}),
+		api.Call(func(_ ...api.Value) api.Value {
+			triggered = true
+			return api.Nil
+		}),
+	)
+}
