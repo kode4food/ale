2f500c22a3eabb17c75dcbcbc63e2ca2503a3f21
Thomas Bradford <thom@kode4food.it>
2020-07-29 12:47:29 +0200

    some refactoring tried to be cute

--- compiler/encoder/call_test.go
+++ compiler/encoder/call_test.go
@@ -12,7 +12,7 @@ func TestCall(t *testing.T) {
 	as := assert.New(t)
 	f1 := func(_ encoder.Type, _ ...data.Value) {}
 	c1 := encoder.Call(f1)
-	as.String("encoder-call", c1.Type())
-	as.Contains(`:type encoder-call`, c1)
+	as.String("encoder", c1.Type())
+	as.Contains(`:type encoder`, c1)
 	as.Contains(`:instance `, c1)
 }
