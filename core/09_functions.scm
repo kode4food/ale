5a6cbc6b47ae9e88264dcc62e699a08d61e2c652
Thomas Bradford <thom@kode4food.it>
2020-08-15 10:55:57 +0200

    adding lazy syntax

--- core/09_functions.scm
+++ core/09_functions.scm
@@ -2,26 +2,26 @@
 
 (define-macro (letfn bindings . body)
   ((lambda-rec parse-bindings (out in)
-     (if (seq in)
-       (let* ([fnList (first in)           ]
-              [fnSym  (first fnList)       ]
-              [fnName (first (rest fnList))]
-              [fnRest (rest (rest fnList)) ])
-         (assert-args
-           (and (is-list fnList)
-                (or (eq fnSym 'lambda-rec)
-                    (eq fnSym 'ale/lambda-rec))
-                (is-local fnName))
-           "bindings must contain named functions")
-         (parse-bindings (append out [fnName fnList]) (rest in)))
-       `(let-rec ,(seq->list out) ,@body)))
+      (if (seq in)
+        (let* ([fnList (first in)           ]
+               [fnSym  (first fnList)       ]
+               [fnName (first (rest fnList))]
+               [fnRest (rest (rest fnList)) ])
+          (assert-args
+            (and (list? fnList)
+                 (or (eq fnSym 'lambda-rec)
+                     (eq fnSym 'ale/lambda-rec))
+                 (local? fnName))
+            "bindings must contain named functions")
+          (parse-bindings (append out [fnName fnList]) (rest in)))
+        `(let-rec ,(seq->list out) ,@body)))
    [] bindings))
 
 (define-lambda partial
   [(func) func]
   [(func . first-args)
      (assert-args
-       (is-apply func) "partial requires a function")
+       (apply? func) "partial requires a function")
      (lambda rest-args
        (apply func (apply append* (cons first-args rest-args))))])
 
@@ -34,11 +34,11 @@
             [first-outer (first funcs)          ]
             [rest-outer  (rest funcs)           ])
        (letfn [(lambda-rec outer (func args rest-funcs)
-                 (if (seq rest-funcs)
-                     (outer (first rest-funcs)
-                            (list func args)
-                            (rest rest-funcs))
-                     (list func args)))]
+                  (if (seq rest-funcs)
+                      (outer (first rest-funcs)
+                             (list func args)
+                             (rest rest-funcs))
+                      (list func args)))]
          `(lambda ,args
             ,(outer first-outer inner rest-outer))))])
 
