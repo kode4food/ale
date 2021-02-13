e006503e4c366d917fa870b482e6647fdb6ef4f5
Thomas Bradford <thom@kode4food.it>
2021-02-13 08:08:50 +0100

    dissoc only returns the resulting sequence

--- core/internal/builtin/object.go
+++ core/internal/builtin/object.go
@@ -1,6 +1,10 @@
 package builtin
 
-import "github.com/kode4food/ale/data"
+import (
+	"errors"
+
+	"github.com/kode4food/ale/data"
+)
 
 // Object creates a new object instance
 var Object = data.Applicative(func(args ...data.Value) data.Value {
@@ -10,6 +14,35 @@ var Object = data.Applicative(func(args ...data.Value) data.Value {
 	}
 	return res
 })
+
+// Get returns a value by key from the provided MappedSequence
+var Get = data.Applicative(func(args ...data.Value) data.Value {
+	s := args[0].(data.MappedSequence)
+	res, _ := s.Get(args[1])
+	return res
+}, 2)
+
+// Assoc returns a new MappedSequence containing the key/value association
+var Assoc = data.Applicative(func(args ...data.Value) data.Value {
+	s := args[0].(data.MappedSequence)
+	if len(args) == 3 {
+		p := data.NewCons(args[1], args[2])
+		return s.Put(p)
+	}
+	if p, ok := args[1].(data.Pair); ok {
+		return s.Put(p)
+	}
+	panic(errors.New(ErrPutRequiresPair))
+}, 2, 3)
+
+// Dissoc returns a new MappedSequence with the key removed
+var Dissoc = data.Applicative(func(args ...data.Value) data.Value {
+	s := args[0].(data.MappedSequence)
+	if _, r, ok := s.Remove(args[1]); ok {
+		return r
+	}
+	return s
+}, 2)
 
 // IsObject returns whether a value is an object
 var IsObject = data.Applicative(func(args ...data.Value) data.Value {
--- core/internal/builtin/sequences.go
+++ core/internal/builtin/sequences.go
@@ -55,35 +55,6 @@ var Nth = data.Applicative(func(args ...data.Value) data.Value {
 	panic(errors.New(ErrIndexOutOfBounds))
 }, 2, 3)
 
-// Get returns a value by key from the provided MappedSequence
-var Get = data.Applicative(func(args ...data.Value) data.Value {
-	s := args[0].(data.MappedSequence)
-	res, _ := s.Get(args[1])
-	return res
-}, 2)
-
-// Assoc returns a new MappedSequence containing the key/value association
-var Assoc = data.Applicative(func(args ...data.Value) data.Value {
-	s := args[0].(data.MappedSequence)
-	if len(args) == 3 {
-		p := data.NewCons(args[1], args[2])
-		return s.Put(p)
-	}
-	if p, ok := args[1].(data.Pair); ok {
-		return s.Put(p)
-	}
-	panic(errors.New(ErrPutRequiresPair))
-}, 2, 3)
-
-// Dissoc returns a new MappedSequence with the key removed
-var Dissoc = data.Applicative(func(args ...data.Value) data.Value {
-	s := args[0].(data.MappedSequence)
-	if v, r, ok := s.Remove(args[1]); ok {
-		return data.NewVector(r, v)
-	}
-	return data.Nil
-}, 2)
-
 // IsSeq returns whether the provided value is a sequence
 var IsSeq = data.Applicative(func(args ...data.Value) data.Value {
 	_, ok := args[0].(data.Sequence)
