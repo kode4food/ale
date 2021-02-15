508f32a55c3be4ed2a60200e1102e9ae332197d5
Thomas Bradford <thom@kode4food.it>
2021-02-15 08:40:06 +0100

    sanity check the smaller ints

--- ffi/int_test.go
+++ ffi/int_test.go
@@ -9,9 +9,19 @@ import (
 	. "github.com/kode4food/ale/internal/assert/helpers"
 )
 
-func TestIntWrapper(t *testing.T) {
+func TestInt64Wrapper(t *testing.T) {
 	as := assert.New(t)
 	f := ffi.MustWrap(func(i1 int32, i2 int64) (int32, int64) {
+		return i1 * 2, i2 * 3
+	}).(data.Function)
+	r := f.Call(I(9), I(15)).(data.Vector).Values()
+	as.Equal(I(18), r[0])
+	as.Equal(I(45), r[1])
+}
+
+func TestInt16Wrapper(t *testing.T) {
+	as := assert.New(t)
+	f := ffi.MustWrap(func(i1 int16, i2 int8) (int16, int8) {
 		return i1 * 2, i2 * 3
 	}).(data.Function)
 	r := f.Call(I(9), I(15)).(data.Vector).Values()
--- ffi/uint_test.go
+++ ffi/uint_test.go
@@ -9,9 +9,19 @@ import (
 	. "github.com/kode4food/ale/internal/assert/helpers"
 )
 
-func TestUIntWrapper(t *testing.T) {
+func TestUInt64Wrapper(t *testing.T) {
 	as := assert.New(t)
 	f := ffi.MustWrap(func(i1 uint32, i2 uint64) (uint32, uint64) {
+		return i1 * 2, i2 * 3
+	}).(data.Function)
+	r := f.Call(I(9), I(15)).(data.Vector).Values()
+	as.Equal(I(18), r[0])
+	as.Equal(I(45), r[1])
+}
+
+func TestUInt16Wrapper(t *testing.T) {
+	as := assert.New(t)
+	f := ffi.MustWrap(func(i1 uint16, i2 uint8) (uint16, uint8) {
 		return i1 * 2, i2 * 3
 	}).(data.Function)
 	r := f.Call(I(9), I(15)).(data.Vector).Values()
