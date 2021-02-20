2874121537fa16121b1f3ee36c30ddd80f985f2a
Thomas Bradford <thom@kode4food.it>
2021-02-20 11:25:49 +0100

    test double anchor

--- compiler/encoder/label_test.go
+++ compiler/encoder/label_test.go
@@ -1,6 +1,7 @@
 package encoder_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/kode4food/ale/runtime/isa"
@@ -27,4 +28,19 @@ func TestLabels(t *testing.T) {
 		isa.New(isa.NoOp),
 		isa.New(isa.Label, 0),
 	}, e.Code())
+}
+
+func TestLabelDoubleAnchor(t *testing.T) {
+	as := NewWrapped(t)
+
+	e := getTestEncoder()
+	l1 := e.NewLabel()
+	l1.DropAnchor()
+
+	defer func() {
+		if rec := recover(); rec == nil {
+			as.Error(errors.New("error not raised on double anchoring"))
+		}
+	}()
+	l1.DropAnchor()
 }
