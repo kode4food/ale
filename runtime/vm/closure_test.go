232a4566d7d0b04540750ec36cf3a7ca1220b7ec
Thomas Bradford <thom@kode4food.it>
2021-10-25 18:41:58 +0200

    covering VM cases

--- runtime/vm/closure_test.go
+++ runtime/vm/closure_test.go
@@ -57,23 +57,9 @@ func TestSimple(t *testing.T) {
 	})
 
 	testResult(t, I(0), []isa.Coder{
-		isa.One,
-		isa.NegOne,
-		isa.Add,
-		isa.Return,
-	})
-
-	testResult(t, I(0), []isa.Coder{
 		isa.Zero,
 		isa.Const, isa.Index(0),
 		isa.Mul,
-		isa.Return,
-	})
-
-	testResult(t, I(-1), []isa.Coder{
-		isa.Zero,
-		isa.One,
-		isa.Sub,
 		isa.Return,
 	})
 
@@ -84,6 +70,65 @@ func TestSimple(t *testing.T) {
 
 	testResult(t, S("arg"), []isa.Coder{
 		isa.Arg, isa.Index(0),
+		isa.Return,
+	})
+}
+
+func TestPopAndDup(t *testing.T) {
+	testResult(t, I(4), []isa.Coder{isa.Two, isa.Dup, isa.Add, isa.Return})
+
+	testResult(t, I(2), []isa.Coder{
+		isa.One, isa.One, isa.Two, isa.Two,
+		isa.Pop, isa.Pop,
+		isa.Add,
+		isa.Return,
+	})
+
+	testResult(t, I(6), []isa.Coder{
+		isa.One, isa.Two, isa.Add,
+		isa.Two, isa.Dup, isa.Pop, isa.Mul,
+		isa.Return,
+	})
+}
+
+func TestReturns(t *testing.T) {
+	testResult(t, data.Nil, []isa.Coder{isa.Nil, isa.Return})
+	testResult(t, I(2), []isa.Coder{isa.Two, isa.Return})
+	testResult(t, data.True, []isa.Coder{isa.True, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.False, isa.Return})
+
+	testResult(t, data.True, []isa.Coder{isa.RetTrue})
+	testResult(t, data.False, []isa.Coder{isa.RetFalse})
+	testResult(t, data.Nil, []isa.Coder{isa.RetNil})
+}
+
+func TestUnary(t *testing.T) {
+	testResult(t, I(-1), []isa.Coder{isa.One, isa.Neg, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.True, isa.Not, isa.Return})
+}
+
+func TestMakeTruthy(t *testing.T) {
+	testResult(t, data.True, []isa.Coder{
+		isa.Const, isa.Index(3),
+		isa.MakeTruthy,
+		isa.Return,
+	})
+
+	testResult(t, data.True, []isa.Coder{
+		isa.True,
+		isa.MakeTruthy,
+		isa.Return,
+	})
+
+	testResult(t, data.False, []isa.Coder{
+		isa.False,
+		isa.MakeTruthy,
+		isa.Return,
+	})
+
+	testResult(t, data.False, []isa.Coder{
+		isa.Nil,
+		isa.MakeTruthy,
 		isa.Return,
 	})
 }
@@ -99,6 +144,41 @@ func TestCalls(t *testing.T) {
 		isa.Add,
 		isa.Return,
 	})
+}
+
+func TestMaths(t *testing.T) {
+	testResult(t, I(3), []isa.Coder{isa.One, isa.Two, isa.Add, isa.Return})
+	testResult(t, I(4), []isa.Coder{isa.Two, isa.Two, isa.Mul, isa.Return})
+	testResult(t, I(-2), []isa.Coder{isa.Two, isa.NegOne, isa.Mul, isa.Return})
+	testResult(t, I(-1), []isa.Coder{isa.One, isa.Two, isa.Sub, isa.Return})
+	testResult(t, R(1, 2), []isa.Coder{isa.One, isa.Two, isa.Div, isa.Return})
+
+	testResult(t, I(1), []isa.Coder{
+		isa.Two, isa.Two, isa.Mul,
+		isa.Two, isa.Mul,
+		isa.One, isa.Add,
+		isa.Two, isa.Mod,
+		isa.Return,
+	})
+}
+
+func TestRelational(t *testing.T) {
+	testResult(t, data.True, []isa.Coder{isa.One, isa.One, isa.Eq, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.One, isa.Two, isa.Eq, isa.Return})
+	testResult(t, data.True, []isa.Coder{isa.Two, isa.One, isa.Neq, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.One, isa.One, isa.Neq, isa.Return})
+
+	testResult(t, data.True, []isa.Coder{isa.One, isa.Two, isa.Lt, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.Two, isa.One, isa.Lt, isa.Return})
+	testResult(t, data.True, []isa.Coder{isa.One, isa.Two, isa.Lte, isa.Return})
+	testResult(t, data.True, []isa.Coder{isa.Two, isa.Two, isa.Lte, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.Two, isa.One, isa.Lte, isa.Return})
+
+	testResult(t, data.True, []isa.Coder{isa.Two, isa.One, isa.Gt, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.One, isa.Two, isa.Gt, isa.Return})
+	testResult(t, data.True, []isa.Coder{isa.Two, isa.One, isa.Gte, isa.Return})
+	testResult(t, data.True, []isa.Coder{isa.Two, isa.Two, isa.Gte, isa.Return})
+	testResult(t, data.False, []isa.Coder{isa.One, isa.Two, isa.Gte, isa.Return})
 }
 
 func TestErrors(t *testing.T) {
