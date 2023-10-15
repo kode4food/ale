b2843826bab08594b6853f8a475ddd6115b98f11
Thomas Bradford <thom@kode4food.it>
2023-10-15 11:25:16 +0200

    in-out relocated

--- ffi/function.go
+++ ffi/function.go
@@ -8,11 +8,6 @@ import (
 )
 
 type (
-	inOutWrappers struct {
-		in  Wrappers
-		out Wrappers
-	}
-
 	funcWrapper struct {
 		*inOutWrappers
 		typ reflect.Type
@@ -94,58 +89,4 @@ func (w *funcWrapper) unwrapVectorCall(c data.Function) makeFuncType {
 		res := c.Call(in...).(data.Vector).Values()
 		return w.out.unwrap(res)
 	}
-}
-
-func makeInOutWrappers(t reflect.Type) (*inOutWrappers, error) {
-	cIn := t.NumIn()
-	in := make(Wrappers, cIn)
-	for i := 0; i < cIn; i++ {
-		w, err := WrapType(t.In(i))
-		if err != nil {
-			return nil, err
-		}
-		in[i] = w
-	}
-	cOut := t.NumOut()
-	out := make(Wrappers, cOut)
-	for i := 0; i < cOut; i++ {
-		w, err := WrapType(t.Out(i))
-		if err != nil {
-			return nil, err
-		}
-		out[i] = w
-	}
-	return &inOutWrappers{
-		in:  in,
-		out: out,
-	}, nil
-}
-
-func (w *inOutWrappers) wrapValueFunction(fn reflect.Value) data.Function {
-	return data.Applicative(func(args ...data.Value) data.Value {
-		in := w.in.unwrap(args)
-		out := fn.Call(in)
-		res, err := w.out[0].Wrap(new(Context), out[0])
-		if err != nil {
-			panic(err)
-		}
-		return res
-	}, len(w.in))
-}
-
-func (w *inOutWrappers) wrapVoidFunction(fn reflect.Value) data.Function {
-	return data.Applicative(func(args ...data.Value) data.Value {
-		in := w.in.unwrap(args)
-		fn.Call(in)
-		return data.Null
-	}, len(w.in))
-}
-
-func (w *inOutWrappers) wrapVectorFunction(fn reflect.Value) data.Function {
-	return data.Applicative(func(args ...data.Value) data.Value {
-		in := w.in.unwrap(args)
-		res := fn.Call(in)
-		out := w.out.wrap(res)
-		return data.NewVector(out...)
-	}, len(w.in))
 }
---
+++ ffi/in-out.go
@@ -0,0 +1,66 @@
+package ffi
+
+import (
+	"reflect"
+
+	"github.com/kode4food/ale/data"
+)
+
+type inOutWrappers struct {
+	in  Wrappers
+	out Wrappers
+}
+
+func makeInOutWrappers(t reflect.Type) (*inOutWrappers, error) {
+	cIn := t.NumIn()
+	in := make(Wrappers, cIn)
+	for i := 0; i < cIn; i++ {
+		w, err := WrapType(t.In(i))
+		if err != nil {
+			return nil, err
+		}
+		in[i] = w
+	}
+	cOut := t.NumOut()
+	out := make(Wrappers, cOut)
+	for i := 0; i < cOut; i++ {
+		w, err := WrapType(t.Out(i))
+		if err != nil {
+			return nil, err
+		}
+		out[i] = w
+	}
+	return &inOutWrappers{
+		in:  in,
+		out: out,
+	}, nil
+}
+
+func (w *inOutWrappers) wrapValueFunction(fn reflect.Value) data.Function {
+	return data.Applicative(func(args ...data.Value) data.Value {
+		in := w.in.unwrap(args)
+		out := fn.Call(in)
+		res, err := w.out[0].Wrap(new(Context), out[0])
+		if err != nil {
+			panic(err)
+		}
+		return res
+	}, len(w.in))
+}
+
+func (w *inOutWrappers) wrapVoidFunction(fn reflect.Value) data.Function {
+	return data.Applicative(func(args ...data.Value) data.Value {
+		in := w.in.unwrap(args)
+		fn.Call(in)
+		return data.Null
+	}, len(w.in))
+}
+
+func (w *inOutWrappers) wrapVectorFunction(fn reflect.Value) data.Function {
+	return data.Applicative(func(args ...data.Value) data.Value {
+		in := w.in.unwrap(args)
+		res := fn.Call(in)
+		out := w.out.wrap(res)
+		return data.NewVector(out...)
+	}, len(w.in))
+}
