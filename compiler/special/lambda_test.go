32f51f77dabb42475661c7478fd9f18b056197bf
Thomas Bradford <thom@kode4food.it>
2023-10-19 08:51:50 +0200

    lambda binding permutations

--- compiler/special/lambda_test.go
+++ compiler/special/lambda_test.go
@@ -5,10 +5,21 @@ import (
 
 	"github.com/kode4food/ale/data"
 	"github.com/kode4food/ale/internal/assert"
+	. "github.com/kode4food/ale/internal/assert/helpers"
 )
 
-func TestEmptyLambda(t *testing.T) {
+func TestLambdaSyntax(t *testing.T) {
 	as := assert.New(t)
 
+	as.EvalTo(`((lambda [(x y) (+ x y)] [(x) (* x 2)]) 20 30)`, I(50))
+	as.EvalTo(`((lambda [(x y) (+ x y)] [(x) (* x 2)]) 20)`, I(40))
+
+	as.EvalTo(
+		`((lambda [(x y) (+ x y)] [(x) (* x 2)] [x x]) 20 30 40)`,
+		V(I(20), I(30), I(40)),
+	)
+
+	as.EvalTo(`((lambda x x) 1 2 3)`, V(I(1), I(2), I(3)))
+	as.EvalTo(`((lambda (x) x) 1)`, I(1))
 	as.EvalTo(`((lambda))`, data.Null)
 }
