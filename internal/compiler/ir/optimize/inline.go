4e13caa8b5bac1dd7487ab761dafa906ef929d8b
Thomas Bradford <thom@kode4food.it>
2024-09-02 21:21:32 +0200

    a bit of cleanup

--- internal/compiler/ir/optimize/inline.go
+++ internal/compiler/ir/optimize/inline.go
@@ -56,10 +56,10 @@ func (m *inlineCallMapper) perform(i isa.Instructions) isa.Instructions {
 
 	argCount := getCallArgCount(i[1])
 	c := m.relabel(p.Code)
-	c = m.getParamCase(c, argCount)
+	c = paramBranchFor(c, argCount)
 	c = m.reindex(p, c)
 	c = m.returns(c)
-	return m.transformArgs(c, argCount)
+	return stackArgs(c, argCount)
 }
 
 func (m *inlineCallMapper) canInline(i isa.Instruction) (*vm.Closure, bool) {
@@ -94,23 +94,6 @@ func (m *inlineCallMapper) relabel(c isa.Instructions) isa.Instructions {
 		res = slices.Insert(res, int(oc), isa.New(isa.Label, labels[oc]))
 	}
 	return res
-}
-
-func (m *inlineCallMapper) getParamCase(
-	c isa.Instructions, argCount isa.Operand,
-) isa.Instructions {
-	b := &visitor.BranchScanner{
-		Then:     visitor.All,
-		Epilogue: visitor.All,
-	}
-	b.Else = b.Scan
-
-	if b, ok := b.Scan(c).(visitor.Branches); ok {
-		if bc := getParamBranch(b, argCount); bc != nil {
-			return bc
-		}
-	}
-	return c
 }
 
 func (m *inlineCallMapper) reindex(
@@ -178,16 +161,6 @@ func (m *inlineCallMapper) addConstant(val data.Value) isa.Operand {
 	return isa.Operand(len(c) - 1)
 }
 
-func (m *inlineCallMapper) transformArgs(
-	c isa.Instructions, argCount isa.Operand,
-) isa.Instructions {
-	res := make(isa.Instructions, 0, len(c)+2)
-	res = append(res, isa.PushArgs.New(argCount))
-	res = append(res, c...)
-	res = append(res, isa.PopArgs.New())
-	return res
-}
-
 func getCallArgCount(i isa.Instruction) isa.Operand {
 	switch i.Opcode() {
 	case isa.Call0:
@@ -219,23 +192,33 @@ func getNextOperand(c isa.Instructions, actOn isa.ActOn) isa.Operand {
 	}
 	return res
 }
+func paramBranchFor(c isa.Instructions, argCount isa.Operand) isa.Instructions {
+	b := &visitor.BranchScanner{
+		Then:     visitor.All,
+		Epilogue: visitor.All,
+	}
+	b.Else = b.Scan
+
+	if b, ok := b.Scan(c).(visitor.Branches); ok {
+		if bc := getParamBranch(b, argCount); bc != nil {
+			return bc
+		}
+	}
+	return c
+}
 
 func getParamBranch(b visitor.Branches, argCount isa.Operand) isa.Instructions {
 	oc, op, ok := isParamCase(b)
 	if !ok {
 		return nil
 	}
-	switch {
-	case oc == isa.NumEq && argCount == op:
+	if oc == isa.NumEq && argCount == op || oc == isa.NumGte && argCount >= op {
 		return b.ThenBranch().Code()
-	case oc == isa.NumGte && argCount >= op:
-		return b.ThenBranch().Code()
-	default:
-		if eb, ok := b.ElseBranch().(visitor.Branches); ok {
-			return getParamBranch(eb, argCount)
-		}
-		return nil
 	}
+	if eb, ok := b.ElseBranch().(visitor.Branches); ok {
+		return getParamBranch(eb, argCount)
+	}
+	return nil
 }
 
 func isParamCase(b visitor.Branches) (isa.Opcode, isa.Operand, bool) {
@@ -252,4 +235,12 @@ func isParamCase(b visitor.Branches) (isa.Opcode, isa.Operand, bool) {
 	oc := p[2].Opcode()
 	op := p[1].Operand()
 	return oc, op, oc == isa.NumEq || oc == isa.NumGte
+}
+
+func stackArgs(c isa.Instructions, argCount isa.Operand) isa.Instructions {
+	res := make(isa.Instructions, 0, len(c)+2)
+	res = append(res, isa.PushArgs.New(argCount))
+	res = append(res, c...)
+	res = append(res, isa.PopArgs.New())
+	return res
 }
