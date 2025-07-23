70d1b80e55a8740f9ae463df37a98af7dea63cc6
Thomas Bradford <thom@kode4food.it>
2025-07-23 07:02:49 +0200

    split out bool

---
+++ data/bool.go
@@ -0,0 +1,67 @@
+package data
+
+import (
+	"math/rand/v2"
+
+	"github.com/kode4food/ale"
+	"github.com/kode4food/ale/internal/lang"
+	"github.com/kode4food/ale/internal/types"
+)
+
+// Bool represents the data True or False
+type Bool bool
+
+const (
+	// True represents the boolean value of True
+	True Bool = true
+
+	// False represents the boolean value of false
+	False Bool = false
+)
+
+var (
+	trueHash  = rand.Uint64()
+	falseHash = rand.Uint64()
+
+	// compile-time checks for interface implementation
+	_ interface {
+		Hashed
+		Procedure
+		ale.Typed
+	} = False
+)
+
+// Call returns the Bool value regardless of the arguments passed
+func (b Bool) Call(...ale.Value) ale.Value {
+	return b
+}
+
+func (b Bool) CheckArity(int) error {
+	return nil
+}
+
+// Equal compares this Bool to another for equality
+func (b Bool) Equal(other ale.Value) bool {
+	return b == other
+}
+
+// String converts this Value into a string
+func (b Bool) String() string {
+	if b {
+		return lang.TrueLiteral
+	}
+	return lang.FalseLiteral
+}
+
+// Type returns the Type for this Bool Value
+func (Bool) Type() ale.Type {
+	return types.BasicBoolean
+}
+
+// HashCode returns the hash code for this Bool
+func (b Bool) HashCode() uint64 {
+	if b {
+		return trueHash
+	}
+	return falseHash
+}
--- data/value_test.go
+++ data/value_test.go
@@ -3,7 +3,6 @@ package data_test
 import (
 	"testing"
 
-	"github.com/kode4food/ale/data"
 	"github.com/kode4food/ale/internal/assert"
 	. "github.com/kode4food/ale/internal/assert/helpers"
 )
@@ -13,23 +12,4 @@ func TestNames(t *testing.T) {
 
 	n := LS("hello")
 	as.Equal(LS("hello"), n)
-}
-
-func TestBools(t *testing.T) {
-	as := assert.New(t)
-
-	as.True(data.True.Equal(data.True))
-	as.False(data.True.Equal(data.False))
-	as.False(data.True.Equal(L()))
-	as.False(data.False.Equal(L()))
-
-	as.String("#t", data.True)
-	as.String("#f", data.False)
-
-	obj := O(
-		C(data.True, S("is true")),
-		C(data.False, S("is false")),
-	)
-	as.String("is true", as.MustGet(obj, data.True))
-	as.String("is false", as.MustGet(obj, data.False))
 }
